2a2712c1fbf76fbe8ddfb58219275559c45c68d3
SlimeyAr <67197922+SLimeyMC@users.noreply.github.com>
2026-05-13 21:36:38 +0700

    Fix missing cast.zig

---
+++ src/directive/cast.zig
@@ -0,0 +1,41 @@
+const std = @import("std");
+const reader = @import("../reader.zig");
+const eval = @import("../eval.zig");
+const Env = @import("../env.zig").Env;
+const Expr = reader.Expr;
+const FnTable = eval.FnTable;
+const Fn = eval.Fn;
+const EvalError = eval.EvalError;
+
+pub fn fnInt(
+	args: []const *Expr,
+	_: *Env,
+	allocator: std.mem.Allocator,
+) EvalError!*Expr {
+	if (args.len != 1) return error.ArityError;
+	const i = try args[0].toInteger();
+	try allocator.free(args[0]);
+	return Expr.integer(allocator, i);
+}
+
+pub fn fnIntOrZero(
+	args: []const *Expr,
+	_: *Env,
+	allocator: std.mem.Allocator,
+) EvalError!*Expr {
+	if (args.len != 1) return error.ArityError;
+	const i = try args[0].toIntegerOrZero();
+	try allocator.free(args[0]);
+	return Expr.integer(allocator, i);
+}
+
+pub fn fnBool(
+	args: []const *Expr,
+	_: *Env,
+	allocator: std.mem.Allocator,
+) EvalError!*Expr {
+	if (args.len != 1) return error.ArityError;
+	const b = try args[0].toBool();
+	try allocator.free(args[0]);
+	return Expr.bool(allocator, b);
+}
